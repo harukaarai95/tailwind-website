13eeb98550aaad97cc11d366070f09d1122eaf4b
Hyejin Leem <jin2im@gmail.com>
2024-09-23 22:47:40 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        '1050' : '1050px',
+      },
+    },
+  },
+  plugins: [],
+}
+
